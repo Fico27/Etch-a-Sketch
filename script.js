d956ece13a3043d840f93dc0b10bb946007ff245
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-24 13:00:18 +0100

    Added in the abilty to make grids based on different user inputs. Added in Flexbox to wrap all the boxes around.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const boxHolder = document.querySelector(".iHoldBoxes")
 
-function boxGenerator(input = 16) {
+function boxGenerator(input = 50) {
 
+input = input * input
     for (let i = 0; i < input; i++) {
         const box = document.createElement("div");
         boxHolder.appendChild(box);
