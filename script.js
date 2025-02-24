9fe4d7e832b1658643bdcd7c4255f4e7b0921c05
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-24 14:56:09 +0100

    added in a column generator to append equal amount of div boxes to

--- script.js
+++ script.js
@@ -2,13 +2,22 @@
 
 const boxHolder = document.querySelector(".iHoldBoxes")
 
+// Makes boxes based on user input
 function boxGenerator(input = 50) {
 
 input = input * input
     for (let i = 0; i < input; i++) {
         const box = document.createElement("div");
-        boxHolder.appendChild(box);
+        
     }
 };
 
-boxGenerator()
+function columnGenerator(num =16){
+    for (let i = 0; i < num; i++){
+        const box = document.createElement("div");
+        box.classList.add("column")
+        boxHolder.appendChild(box);
+    }
+}
+
+columnGenerator()
