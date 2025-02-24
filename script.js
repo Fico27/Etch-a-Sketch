7557eb47f4e35e0380d8c0dc3be2750a39781e36
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-24 15:36:45 +0100

    added in a column generator that will run until count matches user input or num

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 
-
+let num = 16;
+let count = 0;
 const boxHolder = document.querySelector(".iHoldBoxes")
 
-// Makes boxes based on user input
-function boxGenerator(input = 50) {
 
-input = input * input
-    for (let i = 0; i < input; i++) {
-        const box = document.createElement("div");
-        
-    }
-};
-
-function columnGenerator(num =16){
+function columnGenerator(num){
     for (let i = 0; i < num; i++){
         const box = document.createElement("div");
         box.classList.add("column")
         boxHolder.appendChild(box);
     }
+
 }
 
-columnGenerator()
+while (count < num){
+    columnGenerator(num)
+    count++
+}
