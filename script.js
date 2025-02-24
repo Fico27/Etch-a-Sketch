ed0ecaef8a766a5a575d85bce1a03042921d1005
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-24 16:23:05 +0100

    Added in button to change length and width of boxes. Still broken but progress...

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let num = 16;
 let count = 0;
 const boxHolder = document.querySelector(".iHoldBoxes")
+const btn = document.querySelector(".iMakeNewBoxes")
+
 
 
 function columnGenerator(num){
@@ -9,11 +11,20 @@ function columnGenerator(num){
         const box = document.createElement("div");
         box.classList.add("column")
         boxHolder.appendChild(box);
+        box.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "black";
+        })
     }
 
 }
 
-while (count < num){
+
+
+
+btn.addEventListener("click", () => {
+    while (boxHolder.firstChild) {
+        boxHolder.removeChild(boxHolder.lastChild);
+      }
+    num = prompt("Please enter a number up to 100!")
     columnGenerator(num)
-    count++
-}
+})
