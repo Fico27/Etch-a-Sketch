57228fbdae42e7904521b1ac21c05a5448eb4f5c
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-25 14:23:10 +0100

    Fixed js to make the correct amount of rows and columns. Added CSS styles to fix boxes.

--- script.js
+++ script.js
@@ -1,54 +1,39 @@
 
-let num = 16;
+let num = 25;
 let count = 0;
 const boxHolder = document.querySelector(".iHoldBoxes")
 const btn = document.querySelector(".iMakeNewBoxes")
 const box = document.createElement("div");
 
 
-// function columnGenerator(num){
-//     for (let i = 0; i < num; i++){
-//         const box = document.createElement("div");
-//         box.classList.add("column")
-//         boxHolder.appendChild(box);
-//         box.addEventListener("mouseover", (e) => {
-//             e.target.style.backgroundColor = "black";
-//         })
-//     }
 
-// }
 
 function rowGenerator(num) {
     
    
-    for (let i = 1; i < num; i++){
-        const box = document.createElement("div");
+    for (let i = 0; i < num; i++){
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row")
         boxHolder.appendChild(rowContainer)
-        for (let j = 1; j < num; j++){
-            
+        for (let j = 0; j < num; j++){
+            const box = document.createElement("div");
+            box.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "black";
+            })
+            box.classList.add("box")
             rowContainer.appendChild(box)
         }
     }
 }
 
+rowGenerator(num)
 
-while (count < num){
+
+btn.addEventListener("click", () => {
+ 
+    while (boxHolder.firstChild) {
+        boxHolder.removeChild(boxHolder.lastChild);
+      }
+    num = prompt("Please enter a number up to 100!")
     rowGenerator(num)
-    count++
-}
-
-
-// btn.addEventListener("click", () => {
-//     count = 0;
-//     while (boxHolder.firstChild) {
-//         boxHolder.removeChild(boxHolder.lastChild);
-//       }
-//     num = prompt("Please enter a number up to 100!")
-//     columnGenerator(num)
-//     while (count < num){
-//         columnGenerator(num)
-//         count++
-//     }
-// })
+})
