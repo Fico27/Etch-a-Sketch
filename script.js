aa5f6cbc6402a604521fb2bb5d0511fd3c47f8de
Phil <182430206+Fico27@users.noreply.github.com>
2025-02-25 19:41:48 +0100

    Played around with making all boxes fit on screen. Did not get the boxes to all fit on screen without scrolling

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const box = document.createElement("div");
 
 
 function rowGenerator(num) {
-    
-   
-    for (let i = 0; i < num; i++){
+
+
+    for (let i = 0; i < num; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row")
         boxHolder.appendChild(rowContainer)
-        for (let j = 0; j < num; j++){
+        for (let j = 0; j < num; j++) {
             const box = document.createElement("div");
             box.addEventListener("mouseover", (e) => {
                 e.target.style.backgroundColor = "black";
@@ -29,16 +29,26 @@ rowGenerator(num)
 
 
 btn.addEventListener("click", () => {
- 
+
     while (boxHolder.firstChild) {
         boxHolder.removeChild(boxHolder.lastChild);
-      }
+    }
     num = prompt("Please enter a number up to 100!")
-    
 
-    if (num > 100){
+
+    if (num > 100) {
         alert("100 is the max!!")
-    }else {
+    } else if (num > 50 && num < 60) {
+        rowGenerator(num)
+        const boxSizeEdit = document.querySelectorAll(".box");
+        
+            boxSizeEdit.style.height = "10px"
+            boxSizeEdit.style.width = "10px"
+
+       
+    } else {
+        box.style.height = "10px";
+        box.style.width = "10px";
         rowGenerator(num)
     }
 })
